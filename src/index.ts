f13af8d8f891dde82870205e2f000e812ea8f5b4
piska <piska@gmail.com>
2023-12-22 17:16:57 +1000

    Express + Prisma database, registration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient;
+
 
 dotenv.config();
 
@@ -7,7 +11,28 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 const root = process.env.ROOT || './';
 
+app.use(express.urlencoded())
+app.use(express.json());
 app.use(express.static(root));
+
+app.post('/reg', async (req: Request, res: Response) => {
+   const { email, password } = req.body;
+   try {
+   const post = await prisma.user.create({
+       data: {
+           email,
+           password
+       }
+
+   })
+   res.json(post)
+   } catch(e) {
+       console.log(e)
+       console.log({email, password})
+       console.log(req.body)
+       res.json(e)
+   }
+});
 
 app.listen(port, () =>{
     console.log(`[server]: Server is running at http://localhost:${port}`);
