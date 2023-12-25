afafdacae0f10604fa1a7f9eb64edf86887d3b31
Artem <lolod.naugolnov@yandex.ru>
2023-12-25 21:34:22 +1000

    Authorization

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ const temps = process.env.TEMPS || './_temp/'
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static(root));
-app.use(express.static(temps))
+app.use("/temps", express.static(temps))
+app.disable('etag')
 app.post('/reg', async (req: Request, res: Response) => {
    const { email, password } = req.body;
    try {
@@ -24,19 +25,48 @@ app.post('/reg', async (req: Request, res: Response) => {
            email,
            password
        }
-
    })
-   res.json(post)
-       res.redirect(301, "/")
+   res.redirect(301, "/")
    } catch(e) {
        console.log(e)
        console.log({email, password})
        console.log(req.body)
        res.json(e)
-       res.redirect(301,"/")
    }
 });
-
+app.get('/log', async (req: Request, res: Response)=> {
+   const usr_email = '' || req.query.email!.toString();
+   const usr_password = '' || req.query.password!.toString();
+   try{
+       const get = await prisma.user.findFirst({
+           select: {
+               email: true
+           },
+           where: {
+               email : usr_email,
+               password : usr_password
+           }
+       })
+       res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+       res.header("Pragma", "no-cache");
+       res.header("Expires", "0");
+       res.cookie("user", get!.email)
+       console.log(get!.email)
+       res.redirect(301,"/")
+   } catch (e){
+       console.log(e)
+       console.log({usr_email, usr_password})
+       console.log(req.body)
+       res.json(e)
+   }
+});
+app.get('/log-out', (req: Request, res: Response) => {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", "0");
+    res.clearCookie("user");
+    res.redirect(301, "/")
+})
 app.listen(port, () =>{
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- static/assets/app.js
+++ static/assets/app.js
@@ -1,5 +1,14 @@
 'use strict'
 
-import './htmx.min.js'
-
 console.log('PRIVET MIR!!!')
+
+const cookies = document.cookie.split(";")
+const cookie = cookies.find((row) => row.startsWith("user="))?.split("=")[1]
+
+console.log(cookie)
+
+if (cookie !== undefined) {
+    document.getElementById("logName").innerHTML = "<h1>Вы: "+decodeURIComponent(cookie)+"</h1>"
+} else {
+    document.getElementById("logName").innerHTML = null;
+}
